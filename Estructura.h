6e50490c058402371aad3a02f3a2dfb3b69cf260
Werichu <joseangelmagana011@gmail.com>
2025-12-11 14:46:51 -0600

    Estructure sorce

---
+++ Estructura.h
@@ -0,0 +1,11 @@
+#ifndef ESTRUCTURA_H_INCLUDED
+#define ESTRUCTURA_H_INCLUDED
+
+struct Puntos{
+    int contador_jugadas;
+    int victorias_jugadorX;
+    int victorias_jugadorO;
+    int empates;
+};
+
+#endif // ESTRUCTURA_H_INCLUDED
